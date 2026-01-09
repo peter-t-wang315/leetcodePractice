c1e4be9404f1ce7b763a716207ec5f278ac5b187
peter.wang <wang.t.peter@gmail.com>
2026-01-08 20:05:11 -0800

    Updated the majority element solution to have better space complexity.

--- majorityElement.cs
+++ majorityElement.cs
@@ -3,26 +3,34 @@
 // as you have to check each item to make sure you find the majority element.
 
 // __Space complexity__
-// The space complexity of this algorithm is O(n). This is because you create a dictionary that worst case is the size of nums. This is an ok space complexity but I gotta think about the
-// O(1) space complexity solution Leetcode is hinting at.
+// The space complexity of this algorithm is O(1). This is because you only need to create 2 ints which are constant space.
 
 public class Solution {
     public int MajorityElement(int[] nums) {
-        Dictionary<int, int> counts = new Dictionary<int, int>();
-        // Majority number, num occurrences.
-        (int, int) majorityElement = (0, 0);
+        int majorityNum = nums[0];
+        int count = 1;
 
         // Count occurrences of each numbers.
-        foreach (int num in nums)
+        foreach (int num in nums.Skip(1))
         {
-            counts[num] = counts.GetValueOrDefault(num, 0) + 1;
-            // Nice if statement because now we don't have to iterate over the dictionary again saving us like
-            // a little bit of extra time lol.
-            if (majorityElement.Item2 < counts[num]){
-                majorityElement = (num, counts[num]);
+            // If we find another number that matches our current majority num, we add to the count.
+            if (majorityNum == num) {
+                count++;
+            }
+            // If the number we are at is a different number, we need to decrement the counter or restart it.
+            else {
+                // If the majorityNum actually doesn't have the most amount of occurrences, we need to change the majorityNum that's occured.
+                if (count == 0) {
+                    count++;
+                    majorityNum = num;
+                }
+                // If the majorityNum still has the most occurrences, we just decrement it's count.
+                else {
+                    count--;
+                }
             }
         }
 
-        return majorityElement.Item1;
+        return majorityNum;
     }
 }
